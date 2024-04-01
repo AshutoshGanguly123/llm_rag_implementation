316d982eec163e98fe0fd3e399cfad54d4ba5dbc
Ashutosh Ganguly <93579778+AshutoshGanguly123@users.noreply.github.com>
2024-04-01 11:06:58 +0530

    Update generative_gemini.py

--- generative_gemini.py
+++ generative_gemini.py
@@ -2,7 +2,7 @@ import google.generativeai as genai
 import os
 
 #genai.configure(api_key=os.environ['API_KEY'])
-genai.configure(api_key='AIzaSyA90Jp6T4qGE_x0Y3vhV2Qh9bAgr5P94hc')
+genai.configure(api_key='')
 
 def get_summary_gemini(prompt):
     model = genai.GenerativeModel('gemini-pro')
